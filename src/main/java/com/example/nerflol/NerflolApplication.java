a3df23a323dd3949c105a41004e9a966cc96c3a7
Andrew Zhang <zandy99@gmail.com>
2019-10-16 15:08:03 -0400

    added all spring dependencies

---
+++ src/main/java/com/example/nerflol/NerflolApplication.java
@@ -0,0 +1,13 @@
+package com.example.nerflol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NerflolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NerflolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/nerflol/NerflolApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.nerflol;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NerflolApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
